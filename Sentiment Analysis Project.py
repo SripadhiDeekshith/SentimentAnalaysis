69868e700958379ec80ce0f6681e61205335c98c
SripadhiDeekshith <160750984+SripadhiDeekshith@users.noreply.github.com>
2024-04-14 16:41:14 +0530

    Update Sentiment Analysis Project.py

--- Sentiment Analysis Project.py
+++ Sentiment Analysis Project.py
@@ -29,7 +29,7 @@ from sklearn.metrics import confusion_matrix, classification_report
 # Importing the dataset
 DATASET_COLUMNS=['target','ids','date','flag','user','text']
 DATASET_ENCODING = "ISO-8859-1"
-df = pd.read_csv('C:/Talent Battle/4 Weeks ML Project Challenge/Dataset/training.1600000.processed.noemoticon.csv', encoding=DATASET_ENCODING, names=DATASET_COLUMNS)
+df = pd.read_csv("C:\Users\deeks\Downloads\training.1600000.processed.noemoticon.csv", encoding=DATASET_ENCODING, names=DATASET_COLUMNS)
 df.sample(5)
 
 
